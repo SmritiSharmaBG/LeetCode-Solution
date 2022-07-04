ccc3fad385607f5c5845734f3a3d058237172321
Smriti Sharma <77639024+SmritiSharmaBG@users.noreply.github.com>
2022-07-05 01:08:17 +0530

    Swap Nodes in Pairs - Accepted Solution
    Time Complexity : O(n/2)
    Space Complexity : O(1) as no DS is used but O(n/2) is used as aux space

---
+++ Medium/SwapPairs.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode temp = swapPairs(head.next.next);
+        ListNode ret = head.next;
+        head.next.next = head;
+        head.next = temp;
+        return ret;
+    }
+}
