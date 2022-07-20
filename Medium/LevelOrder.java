e51d2684e901d018679d1412f65ad6ca4709ed1c
Smriti Sharma <77639024+SmritiSharmaBG@users.noreply.github.com>
2022-07-20 20:51:44 +0530

    Added alternate solution
    time and complexity is same as above

--- Medium/LevelOrder.java
+++ Medium/LevelOrder.java
@@ -33,4 +33,27 @@ class Solution {
         }
         return ans;
     } 
+    
+    // Alternate solution; time and complexity is same as above
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> ds = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        queue.add(null);
+        while(!queue.isEmpty()) {
+            TreeNode temp = queue.remove();
+            if(temp != null) {
+                ds.add(temp.val);
+                if(temp.left != null) queue.add(temp.left);
+                if(temp.right != null) queue.add(temp.right);
+            }
+            else if(ds.size() != 0) {
+                ans.add(new ArrayList<>(ds));
+                queue.add(null);
+                ds.clear();
+            }
+        }
+        return ans;
+    }
 }
