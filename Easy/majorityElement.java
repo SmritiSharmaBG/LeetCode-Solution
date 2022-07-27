90f1d5537f1e7a669eaa7537ee3af66a5636067d
Smriti Sharma <77639024+SmritiSharmaBG@users.noreply.github.com>
2022-07-27 23:24:40 +0530

    Majority Element - Accepted Solution
    Time Complexity : O(n)
    Space Complexity : O(1)

---
+++ Easy/majorityElement.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int element = -1;
+        for(int i = 0; i < nums.length; i ++) {
+            if (count == 0) element = nums[i];
+            if(nums[i] == element) count ++;
+            else count --;
+        }
+        return element;
+    }
+}
